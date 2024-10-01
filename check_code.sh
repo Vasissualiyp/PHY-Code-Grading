9641d8790a61259051544fb85528afca35f48b11
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-10-01 11:42:48 -0400

    Packaged the grader into nix flake

--- check_code.sh
+++ check_code.sh
@@ -4,15 +4,19 @@
 
 SUBM_DIR="./submissions"
 
+make_dirs=0
+
 student_names=$(ls -lA $SUBM_DIR | awk '{print $9}' | awk -F'_' '{print $1}' | uniq)
 
-for name in $student_names; do
-    # Create a directory for the student if it doesn't exist
-    mkdir -p "$SUBM_DIR/$name"
-    
-    # Move all files matching the student name into their directory
-    #mv "$SUBM_DIR/${name}_*" "$SUBM_DIR/$name/"
-	find "$SUBM_DIR" -type f -name "${name}_*" -exec mv {} "$SUBM_DIR/$name/" \;
+# Move the students' files to their respective dirs
+if [ $make_dirs == 1 ]; then
+    for name in $student_names; do
+        # Create a directory for the student if it doesn't exist
+        mkdir -p "$SUBM_DIR/$name"
+        
+        # Move all files matching the student name into their directory
+    	find "$SUBM_DIR" -type f -name "${name}_*" -exec mv {} "$SUBM_DIR/$name/" \;
+    done
+fi
 
-done
-#echo $student_names
+
---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Computational assignment grading";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (python3.withPackages (ps: with ps; [
+              #healpy # HEALPY IS NOT IN NIXPKGS, SO NEED TO MANUALLY PACKAGE IT!
+              pandas
+              matplotlib
+              numpy
+              scipy
+            ]))
+          ];
+		shellHook = ''
+		echo "Welcome to PHY coding course grading flake!"
+		'';
+        };
+      }
+    );
+}
